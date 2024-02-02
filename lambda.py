b69a3bf8cd4277acd5411a7aa5828cf568219111
EC2 Default User <ec2-user@ip-172-31-12-29.ap-northeast-2.compute.internal>
2024-02-02 10:26:47 +0000

    test

--- lambda.py
+++ lambda.py
@@ -1,7 +1,13 @@
+import boto3
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+    test = event["methodArn"]
+    path = test.split("/")
+    if path[1] == "v1":
+        print("hello")
+    else:
+        print("Access Denied on Path")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from test-Lambda!')
