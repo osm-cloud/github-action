8e4078a55e1bc0aad1931abc42bbad99bb268a43
Your Name <you@example.com>
2024-03-04 20:18:28 +0900

    test

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.10
 from flask import Flask, abort, request, jsonify
 import logging
 
 logging.basicConfig(
-  format = '%(asctime)s %(levelname)s %(message)s',
-  datefmt = '%Y-%m-%d %H:%M:%S',
-  filename = "/var/log/app/app.log",
-  level = logging.INFO
+    format='%(asctime)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    filename="/var/log/app/app.log",
+    level=logging.INFO
 )
 
 app = Flask(__name__)
 
 @app.route('/v1/color', methods=['GET'])
 def get_color():
-  try:
-    ret = {'code': 'red', 'name': 'osm'}
-    return jsonify(ret), 200
-  except Exception as e:
-    logging.error(e)
-    abort(500)
+    try:
+        ret = {'code': 'red', 'name': 'osm'}
+        return jsonify(ret), 200
+    except Exception as e:
+        logging.error(e)
+        abort(500)
 
 @app.route('/health', methods=['GET'])
 def get_health():
-  try:
-    ret = {'status': 'ok'}
-
-    return jsonify(ret), 200
-  except Exception as e:
-    logging.error(e)
-    abort(500)
+    try:
+        ret = {'status': 'ok'}
+        return jsonify(ret), 200
+    except Exception as e:
+        logging.error(e)
+        abort(500)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
